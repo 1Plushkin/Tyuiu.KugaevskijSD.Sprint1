c954a6ab80a01b16096d33bf788afb7fa9f68aae
1Plushkin <Plushkin001@mail.ru>
2025-09-29 22:07:58 +0500

    Add project files.

---
+++ Tyuiu.KugaevskijSD.Sprint1.Task0.V29.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.KugaevskijSD.Sprint1.Task.V29.Test;
+using Tyuiu.KugaevskijSD.Sprint1.Task0.V29.Lib;
+
+[TestClass]
+public sealed class DataServiceTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        DataService ds = new DataService();
+        var res = ds.Calculate();
+        Assert.AreEqual(2, res);
+
+    }
+}
