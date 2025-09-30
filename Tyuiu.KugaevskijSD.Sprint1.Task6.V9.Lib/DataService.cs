616dc08d916a3de3c0a1db39ab2deb49b5abb142
1Plushkin <Plushkin001@mail.ru>
2025-09-30 20:39:39 +0500

    6

--- Tyuiu.KugaevskijSD.Sprint1.Task6.V9.Lib/DataService.cs
+++ Tyuiu.KugaevskijSD.Sprint1.Task6.V9.Lib/DataService.cs
@@ -5,15 +5,19 @@ namespace Tyuiu.KugaevskijSD.Sprint1.Task6.V9.Lib;
 {
     public string MoveLetterToStart(string value)
     {
-        string result = "";
-        foreach (string s in value.Split(" "))
+        string temp = "";
+
+        string[] li = value.Split(' ');
+        foreach (string t in li)
         {
-            List<char> xx = new List<char>((s[s.Length - 1] + s).ToCharArray());
-            xx.RemoveAt(s.Length);
-            result += new string(xx.ToArray()) + " ";
+            string ans = "";
+            ans += t[t.Length - 1];
+            for (int i = 0; i < t.Length - 1; i++)
+            {
+                ans += t[i];
+            }
+            temp += ' ' + ans;
         }
-        List<char> x = new List<char>(result.ToCharArray());
-        x.RemoveAt(result.Length - 1);
-        return new string(x.ToArray());
+        return temp.Trim();
     }
 }
