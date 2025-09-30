b72bb7766ff38da561ea7a52640dec5a7898e94b
1Plushkin <Plushkin001@mail.ru>
2025-09-30 18:10:25 +0500

    1

---
+++ Tyuiu.KugaevskijSD.Sprint1.Task1.V18.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KugaevskijSD.Sprint1.Task1.V18.Test;
+namespace Tyuiu.KugaevskijSD.Sprint1.Task1.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.4;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(-0.6, res);
+        }
+    }
+}
