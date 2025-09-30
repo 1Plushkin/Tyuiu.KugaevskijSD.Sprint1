a3daef192bdef56e69da709c9659eff6841cabdb
1Plushkin <Plushkin001@mail.ru>
2025-09-30 21:18:48 +0500

    7

---
+++ Tyuiu.KugaevskijSD.Sprint1.Task7.V8.Lib/DataService.cs
@@ -0,0 +1,9 @@
+﻿namespace Tyuiu.KugaevskijSD.Sprint1.Task7.V8.Lib;
+using tyuiu.cources.programming.interfaces.Sprint1;
+    public class DataService : ISprint1Task7V8
+    {
+        public double Calculate(double x, double y)
+        {
+            return Math.Round((x * Math.Log(x)) + (y / (Math.Cos(x) - (x / 3))), 3);
+        }
+    }
---
+++ Tyuiu.KugaevskijSD.Sprint1.Task7.V8.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Tyuiu.KugaevskijSD.Sprint1.Task7.V8.Test;
+using Tyuiu.KugaevskijSD.Sprint1.Task7.V8.Lib;
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 3;
+            double z = -1.384;
+
+            Assert.AreEqual(z, ds.Calculate(x, y));
+        }
+    }
+}
