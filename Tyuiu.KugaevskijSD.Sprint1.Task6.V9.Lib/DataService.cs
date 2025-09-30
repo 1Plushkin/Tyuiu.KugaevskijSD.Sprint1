76fee90613b67971fbd85cedc031fc5c4500b706
1Plushkin <Plushkin001@mail.ru>
2025-09-30 20:35:26 +0500

    6

--- Tyuiu.KugaevskijSD.Sprint1.Task6.V9.Lib/DataService.cs
+++ Tyuiu.KugaevskijSD.Sprint1.Task6.V9.Lib/DataService.cs
@@ -1,5 +1,7 @@
-﻿namespace Tyuiu.KugaevskijSD.Sprint1.Task6.V9.Lib;
-    public class DataService : Sprint1Task6V9
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KugaevskijSD.Sprint1.Task6.V9.Lib;
+    public class DataService : ISprint1Task6V9
 {
     public string MoveLetterToStart(string value)
     {
--- Tyuiu.KugaevskijSD.Sprint1.Task6.V9.Lib/Sprint1Task6V9.cs
+++ Tyuiu.KugaevskijSD.Sprint1.Task6.V9.Lib/Sprint1Task6V9.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tyuiu.KugaevskijSD.Sprint1.Task6.V9.Lib
 {
-    public class Sprint1Task6V9
+    public class ISprint1Task6V9
     {
     }
 }
